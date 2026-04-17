7c67717f2739829979d3721809d33e104e8617fc
Anes-BHD <anes.bhd12@gmail.com>
2026-04-17 02:12:25 +0100

    terraform fmt

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "smarthr-terraform-state"
-    key            = "smarthr/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    use_lockfile   = true
+    bucket       = "smarthr-terraform-state"
+    key          = "smarthr/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
 
