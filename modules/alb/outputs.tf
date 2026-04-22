1fba50e56fbe4fe653b3d536e04ccae78e0d1436
Anes-BHD <anes.bhd12@gmail.com>
2026-04-22 18:48:18 +0100

    terraform fmt fix

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -3,4 +3,4 @@ output "alb_dns_name" { value = aws_lb.main.dns_name }
 output "target_group_arn" { value = aws_lb_target_group.app.arn }
 output "alb_sg_id" { value = aws_security_group.alb.id }
 output "waf_arn" { value = aws_wafv2_web_acl.alb.arn }
-output "alb_zone_id"      { value = aws_lb.main.zone_id }
+output "alb_zone_id" { value = aws_lb.main.zone_id }
